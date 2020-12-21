d9f9f49312cb1509a9e2d98d053a8a75a945f077
kulloveth <kulloveth@gmail.com>
2020-12-21 20:51:01 +0100

    fix launch crash

--- app/src/main/java/com/kulloveth/firebaseandroidauthsample/ui/MainActivity.kt
+++ app/src/main/java/com/kulloveth/firebaseandroidauthsample/ui/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.kulloveth.firebaseandroidauthsample.ui.login
+package com.kulloveth.firebaseandroidauthsample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kulloveth.firebaseandroidauthsample.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
